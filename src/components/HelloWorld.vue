8ea7d00cdf8de04f9e80cfaae743f3d44e6afb3a
Steven-Harefa <fairytailtail47@gmail.com>
2021-05-29 12:03:35 +0700

    Todo-FrontEnd

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <h1>Tugas Frontend</h1>
+
+    <ul>
+
+        <li v-for="i in todo">{{i.desc}}</li>
+
+    </ul>
+
+    <input v-model="Teks"/>
+    <br>
+    <br>
+    <button @click="tambah">Simpan Data</button>
+  </div>
+</template>
+
+<script>
+
+  import axios from 'axios'
+  
+  export default {
+
+    data: function(){
+
+      return {
+        todo: [
+          // {desc : 'Argus'},
+          // {desc : 'Alucard'},
+          // {desc : 'Bane'}
+        ],
+
+        Teks : ''
+
+      }
+
+    },
+
+    created: function(){
+
+      axios.get('http://localhost:3000/Input')
+        .then((result) => {
+          this.todo = result.data
+        })
+
+    },
+
+    methods:{
+      tambah: function(){
+        this.todo.push({desc : this.Teks })
+      }
+    }
+
+  }
+
+</script>
+
+
